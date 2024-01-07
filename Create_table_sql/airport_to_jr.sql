323e459a94484e4f9f0540ab0ed881126c5dc2fc
HelloHe110 <128694646+HelloHe110@users.noreply.github.com>
2024-01-07 11:57:14 +0800

    Update airport_to_jr.sql

--- Create_table_sql/airport_to_jr.sql
+++ Create_table_sql/airport_to_jr.sql
@@ -4,10 +4,19 @@
 
 CREATE TABLE IF NOT EXISTS public.airport_to_jr
 (
-    airport_name character varying(100) COLLATE pg_catalog."default",
-    jr_station_name character varying(100) COLLATE pg_catalog."default",
+    airport_iata character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    jr_station_name character varying(100) COLLATE pg_catalog."default" NOT NULL,
     ticket_price_jyp numeric(50,5),
-    duration_minute numeric(50,5)
+    duration_minute numeric(50,5),
+    CONSTRAINT airport_to_jr_pkey PRIMARY KEY (airport_iata, jr_station_name),
+    CONSTRAINT airport_to_jr_airport_iata_fkey FOREIGN KEY (airport_iata)
+        REFERENCES public.airport (iata) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT airport_to_jr_jr_station_name_fkey FOREIGN KEY (jr_station_name)
+        REFERENCES public.jr_station (jr_station) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 )
 
 TABLESPACE pg_default;
