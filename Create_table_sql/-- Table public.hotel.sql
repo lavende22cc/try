a33ed809c1689c13a5b99a0659a47fb29238dff3
HelloHe110 <128694646+HelloHe110@users.noreply.github.com>
2024-01-07 11:55:53 +0800

    Update -- Table public.hotel.sql

--- Create_table_sql/-- Table public.hotel.sql
+++ Create_table_sql/-- Table public.hotel.sql
@@ -4,10 +4,15 @@
 
 CREATE TABLE IF NOT EXISTS public.hotel
 (
-    hotel_name character varying(100) COLLATE pg_catalog."default",
+    hotel_name character varying(100) COLLATE pg_catalog."default" NOT NULL,
     fixed_price numeric(10,0),
     recommendation numeric(10,5),
-    jr_station_mandarin character varying(50) COLLATE pg_catalog."default"
+    jr_station character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT hotel_pkey PRIMARY KEY (hotel_name, jr_station),
+    CONSTRAINT hotel_jr_station_fkey FOREIGN KEY (jr_station)
+        REFERENCES public.jr_station (jr_station) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 )
 
 TABLESPACE pg_default;
